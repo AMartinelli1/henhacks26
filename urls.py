8a1aad3d70af1dd968d06162c93e6d68c7df2966
sxphialin <slin7244@gmail.com>
2026-03-01 10:37:44 -0500

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from tech.views import translate_text   # 👈 adjust if needed
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('translate/', translate_text, name='translate'),
-]
